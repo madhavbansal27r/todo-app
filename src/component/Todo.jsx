9b7d44010c9eb15b6c003b91d76c8d6f6cbda9a5
Madhav Bansal <madhavbansal@example.com>
2026-01-17 12:20:22 +0530

    refactor: improve Todo component state management and form handling

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 
-export default function Todo() {
-    function handleAddTodo() {
+const initialTodo = {
+    title: "",
+    dueDate: "",
+    status: false
+}
 
-        // let todoTitle = document.getElementById('todoTitle').value;
-        console.log("Adding todo...", todoTitle);
+export default function Todo() {
+
+    const [todo, setTodo] = React.useState(initialTodo);
+    function handleAddTodo(event) {
+        event.preventDefault();
+        console.log(
+            `Adding todo... with the title as ${todo.title} and due date as ${todo.dueDate} and the status is ${todo.status}`
+        );
+        setTodo(initialTodo);
     }
 
     return (
         <>
             <div className="">My Todo's</div>
-            <form action="" onSubmit={handleAddTodo()}>
+            <form action="" onSubmit={handleAddTodo}>
+
                 <div className="">
                     <label htmlFor="">Todo Title</label>
-                    <input type="text" name="" placeholder='Enter your todos here' id="todoTitle" />
+                    <input type="text" name="" placeholder='Enter your todos here' id="todoTitle" value={todo.title} onChange={(e) => setTodo({ ...todo, title: e.target.value })} />
                 </div>
 
                 <div className="">
                     <label htmlFor="">Due Date</label>
-                    <input type="date" name="" id="" />
+                    <input type="date" name="" id="todoDueDate" value={todo.dueDate} onChange={(e) => setTodo({ ...todo, dueDate: e.target.value })} />
                 </div>
 
                 <button type="submit">Add Todo</button>
